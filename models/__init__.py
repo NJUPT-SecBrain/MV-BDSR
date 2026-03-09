2295367a664990705a39eafe22aca306c944bc3d
来杯冰美式. <yen01648339636@gmail.com>
2026-03-09 20:42:21 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Models module for GraphCodeBERT and LLM interfaces."""
-
-from .graphcodebert import GraphCodeBERTModel
-from .llm_interface import LLMInterface, LLMError, LLMAuthError
-from .embeddings import EmbeddingModel
-
-__all__ = ["GraphCodeBERTModel", "LLMInterface", "LLMError", "LLMAuthError", "EmbeddingModel"]
